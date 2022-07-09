03eb5bb0abfe8c72edd6d01ffa33c65c95b2d814
u236 <mail@u236.org>
2022-07-09 13:18:58 +0300

    quoted strings in log

--- logger.h
+++ logger.h
@@ -3,8 +3,8 @@
 
 #define LOG_FILE_PATH       "/var/log/homed.log"
 
-#define logInfo             qInfo().noquote()
-#define logWarning          qWarning().noquote()
+#define logInfo             qInfo()
+#define logWarning          qWarning()
 
 #include <QDebug>
 
